ce0f22ed0d5990800e86121a13ec5347433a0d4a
BudgetBytes <holytuta@tutanota.com>
2024-09-01 10:55:50 +0200

    Changed SSL to TLSv1.3. Added instructions on README.md

--- README.md
+++ README.md
@@ -8,7 +8,8 @@ CREATE DATABASE sftj;
 GRANT ALL PRIVILEGES ON sftj.* TO 'dev'@'localhost' WITH GRANT OPTION;
 ```
 
-## SSL setup
+## TLS setup - Change passwords
 ```console
-keytool -genkeypair -alias springboot -keyalg RSA -keysize 4096 -storetype PKCS12 -keystore springboot.p12 -validity 3650 -storepass password
+keytool -genkeypair -alias sftj-tls -keyalg RSA -keysize 4096 -validity 3650 -dname "CN=localhost" -keypass sftj-tls -keystore sftj-tls.p12 -storeType PKCS12 -storepass sftj-tls
+mkdir sslcerts && mv sftj-tls.p12 sslcerts
 ```
--- src/main/java/com/byter/sftj/utils/Jwt.java
+++ src/main/java/com/byter/sftj/utils/Jwt.java
@@ -16,43 +16,35 @@ import io.jsonwebtoken.SignatureAlgorithm;
 
 public class Jwt implements Constants {
 	private static String jwtSecret = getRandomSecret();
-	
-	private static String getRandomSecret() {
-        String CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-        StringBuilder salt = new StringBuilder();
-        Random rnd = new Random();
-        while (salt.length() < 54) {
-            int index = (int) (rnd.nextFloat() * CHARS.length());
-            salt.append(CHARS.charAt(index));
-        }
-        String saltStr = salt.toString();
-        return saltStr;
 
-    }
-	
+	private static String getRandomSecret() {
+		String CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+		StringBuilder salt = new StringBuilder();
+		Random rnd = new Random();
+		while (salt.length() < 54) {
+			int index = (int) (rnd.nextFloat() * CHARS.length());
+			salt.append(CHARS.charAt(index));
+		}
+		String saltStr = salt.toString();
+		return saltStr;
+
+	}
+
 	public static String generate(String username) {
-		System.out.println("SECRET: " + jwtSecret);
 		Key hmacKey = new SecretKeySpec(Base64.getDecoder().decode(jwtSecret), SignatureAlgorithm.HS256.getJcaName());
 		Instant now = Instant.now();
-		String jwtToken = Jwts.builder()
-				.setSubject(username)
-				.setId(UUID.randomUUID().toString())
-				.setIssuedAt(Date.from(now))
-				.setExpiration(Date.from(now.plus(JWT_EXPIRATION, UNIT)))
-				.signWith(hmacKey)
+		String jwtToken = Jwts.builder().setSubject(username).setId(UUID.randomUUID().toString())
+				.setIssuedAt(Date.from(now)).setExpiration(Date.from(now.plus(JWT_EXPIRATION, UNIT))).signWith(hmacKey)
 				.compact();
 		return jwtToken;
 	}
-	
+
 	public static Jws<Claims> validate(String jwtToken) {
 		Key hmacKey = new SecretKeySpec(Base64.getDecoder().decode(jwtSecret), SignatureAlgorithm.HS256.getJcaName());
-		
-		Jws<Claims> claims = Jwts.parserBuilder()
-				.setSigningKey(hmacKey)
-				.build()
-				.parseClaimsJws(jwtToken);
-		
+
+		Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(hmacKey).build().parseClaimsJws(jwtToken);
+
 		return claims;
-	} 
-	
+	}
+
 }
